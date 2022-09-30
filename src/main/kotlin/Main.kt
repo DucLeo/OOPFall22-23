d7abd4478ead29ef35b3816cc550ad95996cc97b
DucLeo <God.Luffy.008@gmail.com>
2022-09-30 08:36:38 +0300

    lab1

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -6,16 +6,20 @@ fun main() {
     val input = readln()
     val request = WikiRequest(input)
 
-    println("\nResults for \"$input\": ")
-    request.results.forEachIndexed { index, result -> println("$index. $result") }
-
-    println("\nEnter number to open: ")
-    try {
-        val index = readln().toInt()
-        request.openWiki(index)
-    } catch (_: NumberFormatException) {
-        println("Entered string is not a number. ")
-    } catch (_: IndexOutOfBoundsException) {
-        println("Index is out of range.")
+    if (request.results.isEmpty()) {
+        println("\nResult for \"$input\" not found.")
+    }
+    else {
+        println("\nResults for \"$input\": ")
+        request.results.forEachIndexed { index, result -> println("$index. $result") }
+        println("\nEnter number to open: ")
+        try {
+            val index = readln().toInt()
+            request.openWiki(index)
+        } catch (_: NumberFormatException) {
+            println("Entered string is not a number.")
+        } catch (_: IndexOutOfBoundsException) {
+            println("Index is out of range.")
+        }
     }
 }
